a8995689649c3a59e9fca1acf9437595f74c66f2
subin <sbsoobin@naver.com>
2021-09-27 15:13:56 +0900

    Feat: swagger ui 상세 설명 추가

--- Nestjs/nest-project/src/cats/cats.controller.ts
+++ Nestjs/nest-project/src/cats/cats.controller.ts
@@ -12,6 +12,7 @@ import {
   UseFilters,
   UseInterceptors,
 } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter';
 import { SuccessInterceptor } from 'src/common/interceptors/success.intercept';
 import { PositiveIntPipe } from 'src/common/pipes/positiveInt.pipe';
@@ -24,26 +25,39 @@ import { CatRequestDto } from './dto/cats.request.dto';
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
+  @ApiOperation({ summary: '현재 고양이 가져오기' })
   @Get()
   getCurrentCat() {
     return 'current cat';
   }
 
+  @ApiResponse({
+    status: 500,
+    description: 'Server Error...',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '성공!',
+  })
+  @ApiOperation({ summary: '회원가입' })
   @Post()
   async signUp(@Body() body: CatRequestDto) {
     return await this.catsService.signUp(body);
   }
 
+  @ApiOperation({ summary: '로그인' })
   @Post('login')
   logIn() {
     return 'login';
   }
 
+  @ApiOperation({ summary: '로그아웃' })
   @Post('logout')
   logOut() {
     return 'logout';
   }
 
+  @ApiOperation({ summary: '고양이 이미지 업로드' })
   @Post('upload/cats')
   uploadCatImg() {
     return 'uploadImg';
--- Nestjs/nest-project/src/cats/dto/cats.request.dto.ts
+++ Nestjs/nest-project/src/cats/dto/cats.request.dto.ts
@@ -1,14 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CatRequestDto {
+  @ApiProperty({
+    example: 'subin@naver.com',
+    description: 'email',
+    required: true,
+  })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    example: 'abc123!',
+    description: 'password',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   password: string;
 
+  @ApiProperty({
+    example: 'subin',
+    description: 'name',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   name: string;
