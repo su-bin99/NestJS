5913162655eb923ddd4e5dff0b1bcaabc2977117
subin <sbsoobin@naver.com>
2021-09-27 15:05:50 +0900

    Feat: swagger ui express 적용

--- Nestjs/nest-project/src/main.ts
+++ Nestjs/nest-project/src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 
@@ -7,6 +8,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  // * swagger ui express 설정
+  const config = new DocumentBuilder()
+    .setTitle('C I C')
+    .setDescription('cat')
+    .setVersion('1.0.0')
+    .build();
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document); //'docs' : swagger api 의 엔드포인트
+
   const PORT = process.env.PORT;
   await app.listen(PORT);
 }
