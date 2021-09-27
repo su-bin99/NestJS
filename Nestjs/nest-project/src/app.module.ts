aa188da1003aa48b8047ed563bfe83cff0cb0f33
subin <sbsoobin@naver.com>
2021-09-27 13:57:22 +0900

    Feat: 몽고디비 연결

--- Nestjs/nest-project/src/app.module.ts
+++ Nestjs/nest-project/src/app.module.ts
@@ -4,9 +4,18 @@ import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { CatsService } from './cats/cats.service';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
