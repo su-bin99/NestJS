cb70336a7389fc933b24cc04647b2d4f4d7c38f7
subin <sbsoobin@naver.com>
2021-09-29 12:00:03 +0900

    Feat: Gateway 생명주기함수 예시

--- 3chatting/src/chats/chats.gateway.ts
+++ 3chatting/src/chats/chats.gateway.ts
@@ -1,13 +1,36 @@
+import { Logger } from '@nestjs/common';
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 
 @WebSocketGateway({ namespace: 'chattings' })
-export class ChatsGateway {
+export class ChatsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private logger = new Logger('chat');
+  constructor() {
+    this.logger.log('constructor');
+  }
+  handleConnection(@ConnectedSocket() socket: Socket) {
+    //클라이언트랑 소켓이 새로 연결되어야지 실행되는 함수
+    this.logger.log(`connected : ${socket.id} ${socket.nsp.name}`);
+  }
+  afterInit() {
+    //게이트웨이가 실행될 때 가장 먼저 실행되는 함수
+    this.logger.log('init');
+  }
+  handleDisconnect(@ConnectedSocket() socket: Socket) {
+    //클라이언트랑 소켓 연결이 끊기고 나서 실행되는 함수
+    this.logger.log(`connected : ${socket.id} ${socket.nsp.name}`);
+  }
+
   @SubscribeMessage('new_user')
   handleNewUser(
     @MessageBody() username: string,
