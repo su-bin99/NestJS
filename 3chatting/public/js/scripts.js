fbb8a031b27ae096175514a7d9baec87cb534dec
subin <sbsoobin@naver.com>
2021-09-29 11:30:42 +0900

    Feat: 소켓통신 예시

--- 3chatting/public/js/scripts.js
+++ 3chatting/public/js/scripts.js
@@ -8,6 +8,15 @@ const formElement = getElementById('chat_form');
 
 function helloUser() {
   const username = prompt('What is your name?');
+  socket.emit('new_user', username, (data) => {
+    console.log(`리턴으로 받아온 값 : ${data}`);
+  });
+  // new_user 가 이벤트 이름
+  // 백엔드 코드에서도 이벤트 이름으로 받아와야함
+
+  socket.on('hello_user', (data) => {
+    console.log(data);
+  });
 }
 
 function init() {
